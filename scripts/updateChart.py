7ea4d3a1aaf9a7ad38d06b8cd431c750a45fec5a
hyeongrok7874 <khr5k0228@gmail.com>
2022-07-29 10:57:40 +0900

    Add updateChart.py

---
+++ scripts/updateChart.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+import json
+
+file_path = './public/data/chart.json'
+
+now = datetime.now()
+
+data = { "hello": str(now) }
+
+with open(file_path, 'w') as outfile:
+    json.dump(data, outfile, indent=2)
