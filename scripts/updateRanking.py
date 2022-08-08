94af8ed5ede880885b34dfd8cccc1ca106fbe36a
hyeongrok7874 <khr5k0228@gmail.com>
2022-08-09 03:25:52 +0900

    Update getRanking

--- scripts/updateRanking.py
+++ scripts/updateRanking.py
@@ -19,7 +19,11 @@ if response.status_code == 200:
 for item in list:
     brand = item.select_one('p.item_title > a').text
     name = item.select_one('p.list_info > a')['title']
-    ranking.append({"brand": brand, "name": name})
+    if(item.select_one('p.price > del')) :
+        unwanted = item.select_one('p.price > del')
+        unwanted.extract()
+    price = item.select_one('p.price').text.strip()
+    ranking.append({"brand": brand, "name": name, "price": price})
 
 
 with open(file_path, 'w') as outfile:
