d11942959f50109224e7d6f9bcb8a42fbf60b542
hyeongrok7874 <khr5k0228@gmail.com>
2022-08-09 03:39:05 +0900

    Add get img

--- scripts/updateRanking.py
+++ scripts/updateRanking.py
@@ -2,28 +2,36 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-url ="https://www.musinsa.com/ranking/best?period=day"
-
 file_path = './public/data/ranking.json'
 
-response = requests.get(url)
-
 ranking = []
+
+select_img = 'div.li_inner > div.list_img > a > img'
+select_brand = 'div.article_info > p.item_title > a'
+select_name = 'div.article_info > p.list_info > a'
+select_price = 'div.article_info > p.price'
+
+url ="https://www.musinsa.com/ranking/best?period=day"
+
+response = requests.get(url)
 
 if response.status_code == 200:
     html = response.text
     soup = BeautifulSoup(html, 'html.parser')
-    list = soup.select('#goodsRankList > li > div.li_inner > div.article_info')[0: 10]
+    list = soup.select('#goodsRankList > li > div.li_inner')[0: 10]
 
+def remove_del (): 
+    if(item.select_one('div.article_info > p.price > del')) :
+        unwanted = item.select_one('div.article_info > p.price > del')
+        unwanted.extract()
 
 for item in list:
-    brand = item.select_one('p.item_title > a').text
-    name = item.select_one('p.list_info > a')['title']
-    if(item.select_one('p.price > del')) :
-        unwanted = item.select_one('p.price > del')
-        unwanted.extract()
-    price = item.select_one('p.price').text.strip()
-    ranking.append({"brand": brand, "name": name, "price": price})
+    remove_del()
+    img = item.select_one(select_img)['data-original']
+    brand = item.select_one(select_brand).text
+    name = item.select_one(select_name)['title']
+    price = item.select_one(select_price).text.strip()
+    ranking.append({"img": img, "brand": brand, "name": name, "price": price})
 
 
 with open(file_path, 'w') as outfile:
