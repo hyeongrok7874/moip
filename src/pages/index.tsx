4b4fab0ed7a068ec29f1fd2e484fee2bc6762d60
hyeongrok7874 <khr5k0228@gmail.com>
2022-08-10 02:28:00 +0900

    Update mainpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,38 @@
 import type { GetStaticProps, NextPage } from "next";
 import axios from "axios";
 import * as S from "styles/Main";
-import Goods from "components/Goods";
+import { Goods, Header } from "components";
 
 interface DataType {
   data: {
+    img: string;
     brand: string;
     name: string;
+    price: string;
   }[];
 }
 
 const Home: NextPage<DataType> = ({ data }) => {
   return (
     <S.MainWrapper>
-      {data.map((item, index) => (
-        <Goods key={index} brand={item.brand} name={item.name} />
-      ))}
+      <Header />
+      <S.GoodsWrapper>
+        {data.map((item, index) => (
+          <Goods
+            key={index}
+            brand={item.brand}
+            name={item.name}
+            img={item.img}
+            price={item.price}
+          />
+        ))}
+      </S.GoodsWrapper>
     </S.MainWrapper>
   );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { data } = await axios.get("http://localhost:3000/data/ranking.json");
+  const { data } = await axios.get("https://moip.vercel.app/data/ranking.json");
   return {
     props: { data },
   };
