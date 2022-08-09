4bae98ca210ddc8dfeb1b8a8b182b994a0265e82
hyeongrok7874 <khr5k0228@gmail.com>
2022-08-10 02:22:50 +0900

    Add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from "next/app";
 import GlobalStyle from "styles/GlobalStyle";
 import React from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>moip - 오늘 모 입지?</title>
+      </Head>
       <Component {...pageProps} />
       <GlobalStyle />
     </>
