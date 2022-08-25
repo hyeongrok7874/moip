fc8e5d69dcb1064e940ccc6ee889e17b6ecc21e3
hyeongrok7874 <khr5k0228@gmail.com>
2022-08-25 10:27:49 +0900

    Add footer style

---
+++ app/src/components/Footer/style.ts
@@ -0,0 +1,40 @@
+import styled from "@emotion/styled";
+
+export const Footer = styled.footer`
+  width: 100%;
+  height: 160px;
+  background: url("images/footerBackground.png") no-repeat center;
+  background-size: cover;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FooterContant = styled.div`
+  width: 190px;
+  height: 75px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Text = styled.p`
+  font-weight: 600;
+  font-size: 16px;
+  color: #070718;
+`;
+
+export const IconBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Link = styled.a``;
+
+export const CopyID = styled.div`
+  cursor: pointer;
+`;
