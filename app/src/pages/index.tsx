e68234546fb1a01290f1d9a050636ae596b9f2d4
hyeongrok7874 <khr5k0228@gmail.com>
2022-08-25 16:55:22 +0900

    Update index page

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,26 +1,16 @@
+import React from "react";
+import { Section1, Section2 } from "Section";
+import { Footer } from "components";
 import type { NextPage } from "next";
-import * as S from "styles/Main";
-import { Goods, Header } from "components";
 import ranking from "./api/ranking.json";
 
 const Home: NextPage = () => {
   return (
-    <S.MainWrapper>
-      <Header />
-      <S.GoodsWrapper>
-        {ranking.map((item, index) => (
-          <Goods
-            key={index}
-            link={item.link}
-            rank={index + 1}
-            brand={item.brand}
-            name={item.name}
-            img={item.img}
-            price={item.price}
-          />
-        ))}
-      </S.GoodsWrapper>
-    </S.MainWrapper>
+    <>
+      <Section1 />
+      <Section2 ranking={ranking} />
+      <Footer />
+    </>
   );
 };
 
