8cc6e1f426743ed36bf9592328332e5828f47e82
hyeongrok7874 <khr5k0228@gmail.com>
2022-08-25 10:26:58 +0900

    Add svgs

---
+++ app/src/Assets/svg/index.tsx
@@ -0,0 +1,4 @@
+export { default as Pointer } from "./Pointer";
+export { default as Instagram } from "./Instagram";
+export { default as Github } from "./Github";
+export { default as Discord } from "./Discord";
