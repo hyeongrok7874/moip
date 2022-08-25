765d88c809c47110341336c06a3a5047c3a97eae
hyeongrok7874 <khr5k0228@gmail.com>
2022-08-25 10:28:51 +0900

    Add dev page

---
+++ app/src/pages/dev.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Section1, Section2 } from "Section";
+import { Footer } from "components";
+import { NextPage } from "next";
+
+const Dev: NextPage = () => {
+  return (
+    <>
+      <Section1 />
+      <Section2 />
+      <Footer />
+    </>
+  );
+};
+
+export default Dev;
