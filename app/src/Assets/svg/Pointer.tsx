aa991088ada45bfbf19341628782d6ef23831944
hyeongrok7874 <khr5k0228@gmail.com>
2022-08-25 10:27:25 +0900

    Add pointer svg

---
+++ app/src/Assets/svg/Pointer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Pointer = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="10" cy="10" r="10" fill="#4A84C8" />
+    <path d="M10 15L4.80385 7.5L15.1962 7.5L10 15Z" fill="black" />
+  </svg>
+);
+
+export default Pointer;
