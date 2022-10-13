fca0722b3a2391ea9eb78e0d0899aece297b1ce2
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-13 10:19:29 +0900

    Add WidthDecorationLine

--- app/src/Section/Section2/index.tsx
+++ app/src/Section/Section2/index.tsx
@@ -2,6 +2,7 @@ import Goods from "components/Goods";
 import React from "react";
 import * as S from "./style";
 import { Rank } from "types/goods";
+import { css } from "@emotion/react";
 
 interface RankingType {
   ranking: Rank[];
@@ -21,6 +22,33 @@ const Section2: React.FC<RankingType> = ({ ranking }) => {
           ))}
         </S.GoodsWrapper>
       </S.Section2Wrap>
+      <S.WidthDecorationLine
+        css={css`
+          width: 94%;
+          top: 50px;
+          left: 0;
+        `}
+      />
+      <S.WidthDecorationLine
+        css={css`
+          width: 84%;
+          top: 680px;
+          left: 0;
+        `}
+      />
+      <S.WidthDecorationLine
+        css={css`
+          width: 12%;
+          top: 1080px;
+          left: 0;
+        `}
+      />
+      <S.WidthDecorationLine
+        css={css`
+          width: 100%;
+          bottom: 150px;
+        `}
+      />
     </S.Section2>
   );
 };
