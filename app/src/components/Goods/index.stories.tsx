46b55e4cdac8fe7d6c4b48c96ae51cc513713a6c
hyeongrok7874 <khr5k0228@gmail.com>
2022-11-10 00:48:00 +0900

    Add Goods story

---
+++ app/src/components/Goods/index.stories.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Goods from ".";
+
+export default {
+  title: "Goods",
+  component: Goods,
+  parameters: {
+    layout: "fullscreen",
+  },
+} as ComponentMeta<typeof Goods>;
+
+const Template: ComponentStory<typeof Goods> = (args) => <Goods {...args} />;
+
+export const basic = Template.bind({});
+
+basic.args = {
+  rank: 1,
+  data: {
+    img: "https://image.msscdn.net/images/goods_img/20220412/2482269/2482269_1_125.jpg",
+    brand: "노스페이스",
+    name: "NJ1DN75A 남성 1996 에코 눕시 자켓",
+    price: "339,000",
+    link: "https://www.musinsa.com/app/goods/2482269?loc=goods_rank",
+  },
+};
