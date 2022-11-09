8c10e9218d142646af9953d394acd957d3fe6968
hyeongrok7874 <khr5k0228@gmail.com>
2022-11-10 00:43:23 +0900

    Update preview

--- app/.storybook/preview.js
+++ app/.storybook/preview.js
@@ -1,9 +1,18 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+import * as NextImage from "next/image";
+import GlobalStyle from "/src/styles/GlobalStyle.tsx";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
