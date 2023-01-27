5d1b8a9a3baa0c82b46dba5e877672c6fcb514d4
hyeongrok7874 <khr5k0228@gmail.com>
2023-01-28 01:32:26 +0900

    Add musinsa link style

---
+++ app/src/components/MusinsaLink/style.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const Link = styled.a`
+  color: #ffffff;
+  font-size: 35px;
+  padding: 10px;
+  cursor: pointer;
+  position: absolute;
+  left: 20px;
+  top: 20px;
+`;
