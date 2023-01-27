eba74e9cd6472ab9af358a56dd5e7140fc72c3bb
hyeongrok7874 <khr5k0228@gmail.com>
2023-01-28 01:31:11 +0900

    Add storybook parameters

--- app/.storybook/preview.js
+++ app/.storybook/preview.js
@@ -16,3 +16,27 @@ export const decorators = [
     </>
   ),
 ];
+
+export const parameters = {
+  backgrounds: {
+    default: "gray",
+    values: [
+      {
+        name: "gray",
+        value: "#F3F5FB",
+      },
+      {
+        name: "white",
+        value: "#FFFFFF",
+      },
+      {
+        name: "black",
+        value: "#000000",
+      },
+      {
+        name: "blue",
+        value: "#2E48A0",
+      },
+    ],
+  },
+};
