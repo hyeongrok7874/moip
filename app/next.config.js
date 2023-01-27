b3e3d003c5e32a21d95d368787a29852a462f76e
hyeongrok7874 <khr5k0228@gmail.com>
2023-01-28 01:30:54 +0900

    Update babel

--- app/next.config.js
+++ app/next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ['image.msscdn.net'],
-  }
-}
+    domains: ["image.msscdn.net"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
