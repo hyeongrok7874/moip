7675293c81740d427c83fb08fca0ac9dfda77ec8
hyeongrok7874 <khr5k0228@gmail.com>
2023-02-08 18:52:20 +0900

    Add meta tags

--- app/src/pages/_document.tsx
+++ app/src/pages/_document.tsx
@@ -21,6 +21,11 @@ class MyDocument extends Document {
             as="style"
             href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard.css"
           />
+          <meta
+            name="description"
+            content="무신사의 일간 랭킹을 가져와 MZ세대의 패션 동향을 파악할 수 있습니다."
+          />
+          <meta name="keyword" content="moip, 모입, musinsa, 무신사" />
         </Head>
         <body>
           <Main />
