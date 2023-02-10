c004daec82999b7dd9df9d428bbb478fb51a015d
hyeongrok7874 <khr5k0228@gmail.com>
2023-02-11 04:41:23 +0900

    Add favicon

--- app/src/pages/_document.tsx
+++ app/src/pages/_document.tsx
@@ -21,6 +21,7 @@ class MyDocument extends Document {
             as="style"
             href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard.css"
           />
+          <link rel="shortcut icon" href="/icons/favicon.png" />
           <meta
             name="description"
             content="무신사의 일간 랭킹을 가져와 MZ세대의 패션 동향을 파악할 수 있습니다."
