921361da936b9039485717c3cce24ef6fa95e767
hyeongrok7874 <khr5k0228@gmail.com>
2023-03-09 15:08:35 +0900

    Add bundle analyzer

--- app/next.config.js
+++ app/next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const nextConfig = withBundleAnalyzer({
   reactStrictMode: true,
   images: {
     domains: ["image.msscdn.net"],
   },
-};
+});
 
 module.exports = nextConfig;
