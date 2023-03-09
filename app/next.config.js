894af5d6aa4677e9bc6febc6df17c04d3bc84994
hyeongrok7874 <khr5k0228@gmail.com>
2023-03-09 15:15:56 +0900

    Revert

--- app/next.config.js
+++ app/next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
-const nextConfig = withBundleAnalyzer({
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["image.msscdn.net"],
   },
-});
+};
 
 module.exports = nextConfig;
