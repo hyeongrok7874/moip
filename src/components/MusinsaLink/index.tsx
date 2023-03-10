d4fc3bfd30e3c528e5fc2a3fef388a9d9009c34e
hyeongrok7874 <khr5k0228@gmail.com>
2023-03-10 12:26:38 +0900

    Update MusinsaLink

--- src/components/MusinsaLink/index.tsx
+++ src/components/MusinsaLink/index.tsx
@@ -1,14 +1,12 @@
-import * as S from "./style";
-import React from "react";
+import * as S from './style';
+import React from 'react';
 
-const MusinsaLink: React.FC = () => {
-  const MUSINSA_URL = "https://www.musinsa.com";
+const MUSINSA_URL = 'https://www.musinsa.com';
 
-  return (
-    <S.Link href={MUSINSA_URL} rel="noreferrer noopener" target="_blank">
-      MUSINSA
-    </S.Link>
-  );
-};
+const MusinsaLink: React.FC = () => (
+  <S.Link href={MUSINSA_URL} rel="noreferrer noopener" target="_blank">
+    MUSINSA
+  </S.Link>
+);
 
 export default MusinsaLink;
