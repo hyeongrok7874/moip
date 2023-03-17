d6dbd63240d25e2491eb2e1b23661b49b4568fc5
hyeongrok7874 <khr5k0228@gmail.com>
2023-03-17 12:07:58 +0900

    Add QueryClientProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,20 @@ import React from "react";
 import Head from "next/head";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <QueryClientProvider client={client}>
       <Head>
         <title>moip - 오늘 모 입지?</title>
       </Head>
       <Component {...pageProps} />
       <GlobalStyle />
       <ToastContainer />
-    </>
+    </QueryClientProvider>
   );
 }
 
