9d92598ee1a79de9134301349626856ecfbb1481
hyeongrok7874 <khr5k0228@gmail.com>
2023-03-21 01:29:45 +0900

    Add apis

---
+++ src/pages/api/nowranking.ts
@@ -0,0 +1,15 @@
+import { RankingType, getNowRanking } from "musinsa";
+import { NextApiRequest, NextApiResponse } from "next";
+import { ResponseError } from "types/api";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<RankingType[] | ResponseError>,
+) {
+  try {
+    const ranking = await getNowRanking();
+    return res.status(200).json(ranking?.slice(0, 10));
+  } catch {
+    return res.status(500).json({ message: "failed to load data" });
+  }
+}
---
+++ src/pages/api/weeklyranking.ts
@@ -0,0 +1,15 @@
+import { RankingType, getWeeklyRanking } from "musinsa";
+import { NextApiRequest, NextApiResponse } from "next";
+import { ResponseError } from "types/api";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<RankingType[] | ResponseError>,
+) {
+  try {
+    const ranking = await getWeeklyRanking();
+    return res.status(200).json(ranking?.slice(0, 10));
+  } catch {
+    return res.status(500).json({ message: "failed to load data" });
+  }
+}
