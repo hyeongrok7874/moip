e86eaee16ef35c3f27da217bd8b9728b66c6a6cf
hyeongrok7874 <khr5k0228@gmail.com>
2023-03-21 15:23:53 +0900

    Add button font-family

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -29,6 +29,13 @@ const GlobalStyle = () => {
         p {
           margin: 0;
         }
+
+        button {
+          font-family: "Pretendard Variable", Pretendard, -apple-system,
+            BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI",
+            "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic",
+            "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+        }
       `}
     />
   );
